3cc6df1a098dfcad647306d34016bb1d6a328a17
adam-94 <ashughes0777@gmail.com>
2022-06-19 19:00:53 +0100

    form test

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -30,17 +30,3 @@ exports.handler = async function (event) {
     statusCode: 200,
   };
 };
-
-export async function getStaticProps() {
-  const notion = new Client({ auth: process.env.NOTION_API_KEY });
-  const response = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID,
-  });
-
-  return {
-    props: {
-      books: response.results,
-    },
-    revalidate: 1,
-  };
-}
--- pages/index.js
+++ pages/index.js
@@ -27,3 +27,17 @@ export default function Home({ books }) {
     </>
   );
 }
+
+export async function getStaticProps() {
+  const notion = new Client({ auth: process.env.NOTION_API_KEY });
+  const response = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID,
+  });
+
+  return {
+    props: {
+      books: response.results,
+    },
+    revalidate: 1,
+  };
+}
