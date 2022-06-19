d1ead48ebc64be05370f6bdd09e4e8efea758e73
adam-94 <ashughes0777@gmail.com>
2022-06-19 18:53:17 +0100

    form test

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Contact from "./contact";
 
-export default function Home() {
+export default function Home({ books }) {
   return (
     <>
       <Head>
@@ -10,6 +10,19 @@ export default function Home() {
       <main>
         <h1>Netlify Forms to Notion</h1>
         <Contact />
+
+        <div className={styles.grid}>
+          {books.map((book) => (
+            <a
+              key={book.id}
+              href="https://nextjs.org/docs"
+              className={styles.card}
+            >
+              <h2>{book.properties.Name.title[0].plain_text} &rarr;</h2>
+              <p>{book.properties.Email.email}</p>
+            </a>
+          ))}
+        </div>
       </main>
     </>
   );
